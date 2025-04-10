a3163a26b4e18a0a0d056308299a7498c6eb6fb7
felip <felipecerboncinix@gmail.com>
2025-04-10 14:28:34 -0300

    Remove event classes

--- Forge/src/main/java/com/cerbon/ml_template/forge/event/ModNameEventsForge.java
+++
@@ -1,9 +0,0 @@
-package com.cerbon.ml_template.forge.event;
-
-import com.cerbon.ml_template.ModName;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(modid = ModName.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class ModNameEventsForge {
-
-}
